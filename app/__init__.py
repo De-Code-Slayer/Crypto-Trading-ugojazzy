9678c8aeadd2f2861cb79dd78771b65b4add230a
emeka <chukwujapheth232@gmail.com>
2023-06-16 22:39:11 +0300

    .

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,21 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# create the extension
+db = SQLAlchemy()
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY='AywjhbiaUHWu.asho:I',    
     )
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.path.join(app.instance_path, 'flaskr.sqlite')
+
+
+    # initialize the app with the extension
+    db.init_app(app)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -23,7 +30,13 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-#  register views
+    # import db models
+    from database.models import User
+
+    with app.app_context():
+        db.create_all()
+
+    #  register views
     from views.front import frontend
     app.register_blueprint(frontend)
 
