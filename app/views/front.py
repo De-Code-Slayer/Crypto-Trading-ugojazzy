f15f0f5034fffd43316b4589c1544a03368e5a23
emeka <chukwujapheth232@gmail.com>
2023-07-02 00:47:14 +0300

    .

--- app/views/front.py
+++ app/views/front.py
@@ -12,6 +12,26 @@ frontend = Blueprint('frontend', __name__, url_prefix='/')
 def home():
     return render_template('landing/index.html')
 
+@frontend.route('/')
+def buy_and_sell():
+    return render_template('landing/index.html')
+
+@frontend.route('/')
+def wallet():
+    return render_template('landing/index.html')
+
+@frontend.route('/')
+def market_data():
+    return render_template('landing/index.html')
+
+@frontend.route('/')
+def contact():
+    return render_template('landing/index.html')
+
+@frontend.route('/')
+def support():
+    return render_template('landing/index.html')
+
 
 @frontend.route('/base')
 def base():
