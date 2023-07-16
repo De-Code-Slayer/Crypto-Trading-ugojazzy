a7300698fa8094aa1d0d39310245adcf5ab11cf8
emeka <chukwujapheth232@gmail.com>
2023-07-16 22:03:52 +0300

    .

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app = create_app()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True, load_dotenv=True)
+    app.run(host='0.0.0.0', port=80, debug=False, load_dotenv=True)
