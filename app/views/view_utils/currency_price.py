67b6345fd1b041e6fdd637034ced5dbfa54612f5
emeka <chukwujapheth232@gmail.com>
2023-07-18 22:57:11 +0300

    cnaged USDT to USD

--- app/views/view_utils/currency_price.py
+++ app/views/view_utils/currency_price.py
@@ -4,6 +4,6 @@ import os
 COIN_LAYER_API_KEY = os.getenv('COIN_LAYER_API_KEY')
 
 def get_usd_to_() -> dict:
-    exchang_rate = requests.get(f'http://api.coinlayer.com/api/live?access_key={COIN_LAYER_API_KEY}&symbols=BTC,ETH,USDT&target=USDT')
+    exchang_rate = requests.get(f'http://api.coinlayer.com/api/live?access_key={COIN_LAYER_API_KEY}&symbols=BTC,ETH,USDT&target=USD')
     return exchang_rate.json().get('rates')
     
