c95483d99c97d3bad51d42d3240c2084780bdbd6
emeka <chukwujapheth232@gmail.com>
2023-09-08 02:38:28 +0300

    current_user

--- app/views/view_utils/payment_method.py
+++ app/views/view_utils/payment_method.py
@@ -1,4 +1,3 @@
-from app.database.models import User, Transactions, TraderProfile
 from flask_login import current_user
 from app import db
 import logging
@@ -10,10 +9,10 @@ def update_payment_method(form_data):
     bitcoin_wallet_address= form_data.get('bitcoin_wallet_address')
 
     try:
-        User.trc_tether_wallet_address = trc_tether_wallet_address
-        User.erc_tether_wallet_address = erc_tether_wallet_address
-        User.ethereum_wallet_address = ethereum_wallet_address
-        User.bitcoin_wallet_address = bitcoin_wallet_address
+        current_user.trc_tether_wallet_address = trc_tether_wallet_address
+        current_user.erc_tether_wallet_address = erc_tether_wallet_address
+        current_user.ethereum_wallet_address = ethereum_wallet_address
+        current_user.bitcoin_wallet_address = bitcoin_wallet_address
 
         db.session.commit()
 
