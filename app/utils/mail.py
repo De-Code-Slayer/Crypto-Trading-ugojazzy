130e53cbbf0c7e5bdb6ee2a41d79de635ce473da
jacheme <chukwujapheth232@gmail.com>
2025-02-27 14:18:57 -0800

    .

--- app/utils/mail.py
+++ app/utils/mail.py
@@ -11,11 +11,6 @@ EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 SENDER_ADDRES  = os.environ.get('SENDER_ADDRES')
 
 
-
-
-
-
-
 # def generate_confirmation_token(email):
 #     serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
 #     return serializer.dumps(email, salt=app.config['SECURITY_PASSWORD_SALT'])
